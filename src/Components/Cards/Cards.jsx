f10d7badf66bd36ef7db4779c8843ae0fdcab69b
shirley-dw <vokacshirley@gmail.com>
2024-09-08 20:35:34 -0300

    New Cards

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -19,7 +19,7 @@ const Cards = () => {
           <h1 className='title-card'>Servicio de traslado para empresas</h1>
           <span className='description-card'>Brindamos atención a todas las empresas que deseen trasladar a sus ejecutivos o empleados, asegurando calidad, seguridad y confort en nuestros servicios.</span>
         </Card.Body>
-       
+          <button className='btn-wsp'><a href="https://wa.me/+541164422009" target="_blank" rel="noreferrer">Solicita tu presupuesto</a></button>
       </Card>
       <Card>    
         <Card.Img variant="top" src="/imagenes/particulares.jpg" className='card-img'/>
@@ -27,14 +27,15 @@ const Cards = () => {
         <h1 className='title-card'>Servicio de traslado para particulares</h1>
         <span className='description-card'>Damos atención a todas las personas que deseen trasladarse de forma privada a cualquier destino del País, asegurando calidad, seguridad y confort.</span>
         </Card.Body>
-       
+        <button className='btn-wsp'><a href="https://wa.me/+541164422009" target="_blank" rel="noreferrer">Contactate</a></button>
       </Card>
       <Card>
         <Card.Img variant="top" src="/imagenes/turismo.jpg" className='card-img'/>
         <Card.Body>
-        <h1 className='title-card'>Servicio de turismo nacional</h1>
-        <span className='description-card'>Ofrecemos servicios de turismo nacional para aquellos que deseen explorar las maravillas de nuestro país. Garantizamos calidad, seguridad y confort en cada uno de nuestros viajes, asegurando una experiencia inolvidable.</span>
+        <h1 className='title-card'>Servicio de traslado turismo nacional</h1>
+        <span className='description-card'>Ofrecemos servicios de turismo nacional para aquellos que deseen explorar las maravillas de nuestro país, asegurando una experiencia inolvidable.</span>
         </Card.Body>
+        <button className='btn-wsp'><a href="https://wa.me/+541164422009" target="_blank" rel="noreferrer">Cronograma de viajes</a></button>
       </Card>
     </CardGroup>
     </div>
--- src/Pages/AboutMe/AboutMe.jsx
+++ src/Pages/AboutMe/AboutMe.jsx
@@ -1,27 +1,57 @@
 //Importo librerias
-import React from 'react'
+import React from 'react';
+import { useEffect,useState } from 'react';
+import chroma from 'chroma-js';
 //Importo estilos
 import './AboutMe.css'
-//Importo componentes
-
 
 const AboutMe = () => {
+   
+    const [aboutMe, setAboutMe] = useState({
+        Puntualidad: 80,
+        Seguridad: 90,
+        Responsabilidad: 95,
+        Profesionalismo: 85,
+        Compromiso: 90,
+        Respeto: 95,
+        Confort: 90
+    })
+    const colors = chroma.scale(['#F36D00', '#4E98E7', '#5AFF5A', ]).mode('lab').colors(Object.keys(aboutMe).length);
+    useEffect(() => {
+        
+    })
     return (
         <div className='about-container'>
-            <div className='about-content'>   
-            <div className='about-company'>
-        
-            <h1 className='title-about'>Empresa</h1>
-            <span className='description-about'>Somos una agencia de viajes dedicada a crear momentos inolvidables junto a nuestros pasajeros.</span>
+            <div className='about-content'>
+                <h1 className='title-about'>Empresa</h1>
+                <div className='about-section'>
+                    <div className='about-company'>
+                        <h2 className='subtitle-about'>Nosotros</h2>
+                        <span className='description-about'>Somos una agencia de viajes dedicada a crear momentos inolvidables para nuestros clientes.</span>
+                        <span className='description-about'>Trabajando hace mas de 25 años en traslado de pasajeros aseguramos la calidad, el confort y la seguridad en cada uno de nuestros viajes.</span>
+                        <span className='description-about'>Sur Bus se destaca por ofrecer un Servicio Puntual, realizando traslado y recepción de pasajeros en los distintos aeropuertos del país y terminales de ömnibus. </span>
+                        <span className='description-about'>En nuestra agencia de viajes contamos con personal altamente calificado y con experiencia en el servicio de traslado de pasajeros. </span>
+                    </div>         
+                        <ul className='caracteristics'>
+                            {Object.keys(aboutMe).map((key, index) => (
+                                <li key={index} className='li-caracteristics'>
+                                    <div className='progress-bar'>
+                                        <div className='progress' style={{ width: `${aboutMe[key]}%`, backgroundColor: `${colors[index]}` }}>
+                                            <h3 className='caracteristic-title'>{key}</h3>
+                                            <p className='description-caracteristics'>{aboutMe[key]}%</p>
+                                        </div>
+                                    </div>
+                                </li>
+                            ))}
+                        </ul>
+                   
+                </div>
             </div>
-            </div>
-                 </div>
-                
+        </div>
     )
 }
 
 export default AboutMe
-
 /*   <div className='about-company'>
             <h1 className='title-about'>Empresa</h1>
             <span className='description-about'>Somos una agencia de viajes dedicada a crear momentos inolvidables junto a nuestros pasajeros.</span>
